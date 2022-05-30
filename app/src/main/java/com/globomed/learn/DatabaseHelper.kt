4d3c5342ea51fddcff916592d10c61234b3cf5e9
Ndaboom <ndabosam084@gmail.com>
2022-05-30 10:18:57 -0400

    Crud employee table

---
+++ app/src/main/java/com/globomed/learn/DatabaseHelper.kt
@@ -0,0 +1,21 @@
+package com.globomed.learn
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseHelper(context: Context?) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(GloboMedDbContract.EmployeeEntry.SQL_CREATE_ENTRIES)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.execSQL(GloboMedDbContract.EmployeeEntry.ALTER_TABLE_1)
+    }
+
+    companion object {
+        const val DATABASE_NAME = "globomed.db"
+        const val DATABASE_VERSION = 2
+    }
+}
